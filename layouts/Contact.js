cd119fca601cd80f33d5e384f2c4bfdc6eb15d91
André Abreu de Sá <andre.abreu.ufmg@gmail.com>
2023-09-17 14:06:48 +0000

    "Feat: contact email, funcionando"

--- layouts/Contact.js
+++ layouts/Contact.js
@@ -1,10 +1,82 @@
 import config from "@config/config.json";
 import Banner from "./components/Banner";
 import ImageFallback from "./components/ImageFallback";
+import { useState } from "react";
 
 const Contact = ({ data }) => {
   const { frontmatter } = data;
   const { title } = frontmatter;
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
+  //   Form validation state
+  const [errors, setErrors] = useState({});
+
+  //   Setting button text on form submission
+  const [buttonText, setButtonText] = useState("Send");
+
+  // Setting success or failure messages states
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [showFailureMessage, setShowFailureMessage] = useState(false);
+
+  // Validation check method
+  const handleValidation = () => {
+    let tempErrors = {};
+    let isValid = true;
+
+    if (name.length <= 0) {
+      tempErrors["name"] = true;
+      isValid = false;
+    }
+    if (email.length <= 0) {
+      tempErrors["email"] = true;
+      isValid = false;
+    }
+    if (subject.length <= 0) {
+      tempErrors["subject"] = true;
+      isValid = false;
+    }
+    if (message.length <= 0) {
+      tempErrors["message"] = true;
+      isValid = false;
+    }
+
+    setErrors({ ...tempErrors });
+    console.log("errors", errors);
+    return isValid;
+  };
+
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    let isValidForm = handleValidation();
+
+     
+      const res = await fetch("/api/sendgrid", {
+        body: JSON.stringify({
+          email: email,
+          name: name,
+          subject: subject,
+          message: message,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+      });
+
+      const { error } = await res.json();
+      if (error) {
+        console.log(error);
+        return;
+      }
+    console.log(name, email, subject, message);
+  };
 
   return (
     <section className="section">
@@ -25,6 +97,7 @@ const Contact = ({ data }) => {
               method="POST"
               action={config.params.contact_form_action}
               className="contact-form rounded-xl p-6 shadow-[0_4px_25px_rgba(0,0,0,0.05)]"
+              onSubmit={handleSubmit}
             >
               <h2 className="h4 mb-6">Send A Message</h2>
               <div className="mb-6">
@@ -39,6 +112,8 @@ const Contact = ({ data }) => {
                   name="name"
                   placeholder="Nome"
                   type="text"
+                  value={name}
+                  onChange={(e) => {setName(e.target.value)}}
                   required
                 />
               </div>
@@ -54,6 +129,8 @@ const Contact = ({ data }) => {
                   name="email"
                   placeholder="Email"
                   type="email"
+                  value={email}
+                  onChange={(e) => {setEmail(e.target.value)}}
                   required
                 />
               </div>
@@ -68,6 +145,8 @@ const Contact = ({ data }) => {
                   className="form-input w-full"
                   name="subject"
                   type="text"
+                  value={subject}
+                  onChange={(e) => {setSubject(e.target.value)}}
                   required
                 />
               </div>
@@ -78,9 +157,12 @@ const Contact = ({ data }) => {
                 >
                   Mensagem
                 </label>
-                <textarea className="form-textarea w-full" rows="6" />
+                <textarea className="form-textarea w-full" rows="6" 
+                value={message}
+                onChange={(e) => {setMessage(e.target.value)}}
+                />
               </div>
-              <button className="btn btn-primary block w-full">
+              <button type={"submit"} className="btn btn-primary block w-full">
                 Enviar
               </button>
             </form>
